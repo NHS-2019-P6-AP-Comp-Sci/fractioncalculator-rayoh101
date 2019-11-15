391ab9cec0a3f96d781319e5ad896e9691c2d086
s-ohr <s-ohr@53S50023202.it.bsd405.org>
2019-11-15 13:19:25 -0800

    Turning in checkpoint 1

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,29 +4,30 @@
 
 package fracCalc;
 
+import java.util.Scanner;
+
+import org.junit.Test;
+
 public class FracCalc {
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+    	public static void main(String[] args) {
+    		Scanner s = new Scanner(System.in);
+    		System.out.println("Enter fraction problem: ");
+    		String userResponse = s.nextLine();
+    		System.out.println(produceAnswer(userResponse));
+    		
+    	}
+ 
 
-    }
-
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input)
     {
-        // TODO: Implement this function to produce the solution to the input
-
-        return "";
+		Scanner s = new Scanner(input);
+		String operand1 = s.next();
+		String operator = s.next();
+		String operand2 = s.next();
+		
+		return operand2;
     }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
 
 }
